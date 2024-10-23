7d7af08a0358f85e07db1354c99db0f512bafb32
Patricia <patricia.badji89@gmail.com>
2024-10-23 10:53:55 +0200

    updates

--- JS/welcomePage.js
+++ JS/welcomePage.js
@@ -4,14 +4,16 @@ buttonProceed.disabled = true
 const checkBox = document.getElementById('checkBox')
 checkBox.addEventListener('change', function () {
   if (this.checked) {
-    buttonProceed.disabled = false // Abilita il bottone
-    buttonProceed.classList.add('activatedButton') // Aggiungi la classe per l'illuminazione
+    buttonProceed.disabled = false
+    buttonProceed.classList.add('activatedButton')
   } else {
-    buttonProceed.disabled = true // Disabilita il bottone
-    buttonProceed.classList.remove('activatedButton') // Rimuovi la classe per l'illuminazione
+    buttonProceed.disabled = true
+    buttonProceed.classList.remove('activatedButton')
   }
 })
-
+if (buttonProceed.classList.contains('activatedButton')) {
+  buttonProceed.classList.remove('activatedButton')
+}
 document.getElementById('buttonProceed')
 buttonProceed.addEventListener('click', function () {
   location.href = 'questionPage.html'
