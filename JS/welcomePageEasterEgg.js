f16d532f173ff1a507104250268ff6a4e06fd4e0
Lorenz <l.lione@remoteitalia.com>
2024-10-23 18:34:27 +0200

    Aggiornamenti vari
    Aggiornamento del sistema di selezione del tema.
    Modifica dei CSS.
    Aggiustamenti e standardizzazione del codice

--- JS/welcomePageEasterEgg.js
+++ JS/welcomePageEasterEgg.js
@@ -13,11 +13,27 @@
 // ***********************************************************************
 //
 
-// definisce il div che contiene il testo delle domande
-sectionTitle = document.getElementById('title')
-sectionContent = document.getElementById('content')
-sectionButton = document.getElementById('button')
-sectionLastSection = document.getElementById('lastSection')
+// Trova il DIV in cui inserrire l'immagine dell'icona
+const divLogo = document.getElementById('divLogo')
+
+// Per trovare i topics unici, uso il metodo 'filter' sul metodo 'map' che mi restituisce un array con tutti i topics
+// https://stackoverflow.com/questions/9229645/remove-duplicate-values-from-js-array
+let topics = questionsWithImage
+  .map((question) => question.topic)
+  .filter((item, pos, array) => {
+    return !pos || item != array[pos - 1]
+  })
+
+// topic è l'argomento delle domande deve essere espresso come stringa ed
+// essere ASSOLUTAMENTE identico al 'topic' riportato nel file 'questions.js'
+// se non è settato nel localStore, usa il valore di default
+let topic =
+  localStorage.getItem('examTopic') !== null
+    ? localStorage.getItem('examTopic')
+    : 'HTML, CSS, JS' // Sempre il solito valore di default
+
+// Istanzia la variabile che definisce il tema (è il topic in minuscolo)
+let theme = topic.toLocaleLowerCase()
 
 //
 // ***********************************************************************
@@ -27,13 +43,24 @@ sectionLastSection = document.getElementById('lastSection')
 // ***********************************************************************
 //
 
-// Per trovare i topics unici, uso il metodo 'filter' sul metodo 'map' che mi restituisce un array con tutti i topics
-// https://stackoverflow.com/questions/9229645/remove-duplicate-values-from-js-array
-let topics = questionsWithImage
-  .map((question) => question.topic)
-  .filter((item, pos, array) => {
-    return !pos || item != array[pos - 1]
-  })
+// Al caricamento dell pagina, controlla se il tema è già stato settata e nel caso lo carica
+// Se il tema è 'html, css, js' non carica nessun file CSS
+if (theme !== 'html, css, js' && !document.getElementById(theme)) {
+  var head = document.getElementsByTagName('head')[0]
+  var link = document.createElement('link')
+  link.rel = 'stylesheet'
+  link.type = 'text/css'
+  link.href = `./assets/CSS/_${theme}.css`
+  link.media = 'all'
+  head.appendChild(link)
+
+  // Crea un'immagine dopo il logo di EPICODE
+  const image = document.createElement('img')
+  image.id = 'themeIcon'
+  image.src = `./assets/img/icon${theme}.png`
+  image.alt = 'icon'
+  divLogo.appendChild(image)
+}
 
 // Crea il selettore per i topics
 const selectElement = document.createElement('select')
@@ -45,6 +72,8 @@ topics.forEach((topic) => {
   const option = document.createElement('option')
   option.value = topic
   option.text = topic
+  // E il 'topic' selezionato è quello che è stato settato lo seleziona
+  topic.toLocaleLowerCase() === theme ? (option.selected = true) : null
   selectElement.appendChild(option)
 })
 
@@ -52,21 +81,8 @@ topics.forEach((topic) => {
 sectionLastSection.appendChild(selectElement)
 
 // Configura un valore di default nel localStorage e attiva un eventListener se il dopic viene modificato
-localStorage.setItem('examTopic', 'HTML, CSS, JS')
+// e ricarica la pagina
 selectElement.addEventListener('change', (e) => {
   localStorage.setItem('examTopic', e.target.value) // Non deve essere minuscolo perché nell'array delle domande è con l'iniziale maiuscola
-  theme = e.target.value.toLocaleLowerCase()
-
-  if (theme !== 'HTML, CSS, JS' && !document.getElementById(theme)) {
-    var head = document.getElementsByTagName('head')[0]
-    var link = document.createElement('link')
-    link.rel = 'stylesheet'
-    link.type = 'text/css'
-    link.href = `./assets/CSS/_${theme}.css`
-    link.media = 'all'
-    head.appendChild(link)
-  }
-  if (theme === 'HTML, CSS, JS') {
-    document.getElementById(theme).remove()
-  }
+  location.reload()
 })
