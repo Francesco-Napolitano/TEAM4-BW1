57f866be114b1357ef9493342a75bf2e83cacd2e
Francesco-Napolitano <francesco.46napolitano@gmail.com>
2024-10-23 16:26:35 +0200

    modify feedback.js

--- JS/feedbackPage.js
+++ JS/feedbackPage.js
@@ -15,3 +15,29 @@ stars.addEventListener('click', (e) => {
     starColoured.classList.add('colorStar')
   }
 })
+
+const nextQuestionButton = document.getElementById('nextQuestionButton')
+const allStars = document.querySelectorAll('#star i')
+const reviewInput = document.getElementById('review')
+
+nextQuestionButton.disabled = true
+
+// Variabile per tracciare se una valutazione è stata selezionata
+let ratingSelected = false
+
+allStars.forEach((star) => {
+  star.addEventListener('click', () => {
+    ratingSelected = true
+  })
+})
+
+// Abilita o disabilita il bottone in base alla presenza del feedback
+reviewInput.addEventListener('input', () => {
+  if (ratingSelected && reviewInput.value.trim() !== '') {
+    nextQuestionButton.disabled = false
+    nextQuestionButton.classList.add('activatedButton')
+  } else {
+    nextQuestionButton.disabled = true
+    nextQuestionButton.classList.remove('activatedButton')
+  }
+})
